8f0fdca7f21881fb69e2e6d3157e6a75e8508633
Michael Nelson <michael@eventespresso.com>
2015-05-13 17:00:58 -0700

    supposedly this should allow it to be html

--- templates/shortcode_body.template.php
+++ templates/shortcode_body.template.php
@@ -3,7 +3,7 @@
 	<div ng-controller="mycontroller">
 		<article ng-repeat="event in events">
 			<h3>Event Name: {{ event.EVT_name }}</h3>
-			<p>Description: {{ event.EVT_desc }}</p>
+			<p ng-bind-html="event.EVT_desc"></p>
 			<h4>Datetimes:</h4>
 			<ul>
 				<li ng-repeat="datetime in event.datetimes">
