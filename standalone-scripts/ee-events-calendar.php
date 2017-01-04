54f3e942d35b31488aa2c94fd4d2799197952f00
Mike Nelson <michael@eventespresso.com>
2017-01-03 20:31:57 -0800

    [touch:2358]{t:10}fix bad json date some more. dont forget the T

--- standalone-scripts/ee-events-calendar.php
+++ standalone-scripts/ee-events-calendar.php
@@ -17,10 +17,10 @@
 				var objectdata = {
                         where: {
                             'Datetime.DTT_EVT_end': [
-                                '>', start.format() + '00:00:00'
+                                '>', start.format() + 'T00:00:00'
                             ],
                             'Datetime.DTT_EVT_start': [
-                                '<', end.format() + '00:00:00'
+                                '<', end.format() + 'T00:00:00'
                             ]
                         },
 						"include": 'Datetime.*'
